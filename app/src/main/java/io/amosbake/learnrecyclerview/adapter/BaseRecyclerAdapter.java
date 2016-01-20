9285c3f78de233044ae04d81398183f6181730b8
严昊 <amosbake@gmail.com>
2016-01-20 19:06:16 +0800

    RecyclerAdapter 基类

---
+++ app/src/main/java/io/amosbake/learnrecyclerview/adapter/BaseRecyclerAdapter.java
@@ -0,0 +1,86 @@
+package io.amosbake.learnrecyclerview.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * RecyclerAdapter 的基类, 继承需要实现其绑定数据的方法
+ * Author: yanhao(amosbake@gmail.com)
+ * Date : 2015-10-12
+ * Time: 15:31
+ */
+public abstract class BaseRecyclerAdapter<T> extends RecyclerView.Adapter<RecyclerHolder> {
+    protected List<T> datas;
+    private final int itemLayoutId;
+    private boolean isScrolling;
+    protected final Context context;
+
+    public List<T> getDatas() {
+        return datas;
+    }
+
+    public BaseRecyclerAdapter(RecyclerView v, Collection<T> datas, int itemLayoutId) {
+        if (datas == null) {
+            this.datas = new ArrayList<>();
+        } else if (datas instanceof List) {
+            this.datas = (List<T>) datas;
+        } else {
+            this.datas = new ArrayList<>(datas);
+        }
+        this.itemLayoutId = itemLayoutId;
+        this.context = v.getContext();
+        v.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                isScrolling = !(newState == RecyclerView.SCROLL_STATE_IDLE);
+            }
+        });
+    }
+
+    /**
+     * RecyclerView 适配器数据填充方法
+     *
+     * @param holder      viewholder
+     * @param item        javabean
+     * @param position    position
+     * @param isScrolling 是否正在滚动
+     */
+    protected abstract void convert(RecyclerHolder holder, T item, int position, boolean isScrolling);
+
+    @Override
+    public RecyclerHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new RecyclerHolder(LayoutInflater.from(context).inflate(itemLayoutId, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerHolder holder, int position) {
+        convert(holder, datas.get(position), position, isScrolling);
+    }
+
+    @Override
+    public int getItemCount() {
+        return datas.size();
+    }
+
+
+
+    public BaseRecyclerAdapter<T> refresh(Collection<T> datas) {
+        if (datas == null) {
+            this.datas = new ArrayList<>();
+        } else if (datas instanceof List) {
+            this.datas = (List<T>) datas;
+        } else {
+            this.datas = new ArrayList<>(datas);
+        }
+        return this;
+    }
+
+
+}
---
+++ app/src/main/java/io/amosbake/learnrecyclerview/adapter/RecyclerHolder.java
@@ -0,0 +1,129 @@
+package io.amosbake.learnrecyclerview.adapter;
+
+import android.graphics.Bitmap;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.SparseArray;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * 适配RecycleView.ViewHolder的基类
+ * Author: yanhao(amosbake@gmail.com)
+ * Date : 2015-10-12
+ * Time: 15:35
+ */
+public class RecyclerHolder extends RecyclerView.ViewHolder {
+    private static final String TAG = "RecyclerHolder";
+    private final SparseArray<View> mViews;
+    private final static int MAX_VIEW_NUMBER = 8;//布局中有id的view数 不超过8个
+
+    public RecyclerHolder(View itemView) {
+        super(itemView);
+        this.mViews = new SparseArray<>(MAX_VIEW_NUMBER);
+    }
+
+    public <T extends View> T getView(int viewId) {
+        View view = mViews.get(viewId);
+        if (view == null) {
+            view = itemView.findViewById(viewId);
+            mViews.put(viewId, view);
+        }
+        return (T) view;
+    }
+
+    public RecyclerHolder setText(int viewId, @NonNull String text) {
+        TextView view = getView(viewId);
+        view.setText(text);
+        return this;
+    }
+
+
+    public RecyclerHolder setImage(int viewId, int imgResId) {
+        ImageView imageView = getView(viewId);
+        imageView.setImageResource(imgResId);
+        return this;
+    }
+
+    public RecyclerHolder setImage(int viewId, @NonNull Drawable drawable) {
+        ImageView imageView = getView(viewId);
+        imageView.setImageDrawable(drawable);
+        return this;
+    }
+
+    public RecyclerHolder setImage(int viewId, @NonNull Bitmap bitmap) {
+        ImageView imageView = getView(viewId);
+        imageView.setImageBitmap(bitmap);
+        return this;
+    }
+
+
+
+    /**
+     * 设置控件的左margin
+     * @param xPos 左margin像素大小
+     */
+    public RecyclerHolder setXPos(int viewId, int xPos){
+        View view = getView(viewId);
+        ViewGroup.LayoutParams lp = view.getLayoutParams();
+        if(lp instanceof RelativeLayout.LayoutParams){
+            RelativeLayout.LayoutParams tLp = (RelativeLayout.LayoutParams)lp;
+            tLp.setMargins(xPos, tLp.topMargin, tLp.rightMargin, tLp.bottomMargin);
+        }
+        if(lp instanceof LinearLayout.LayoutParams){
+            LinearLayout.LayoutParams tLp = (LinearLayout.LayoutParams)lp;
+            tLp.setMargins(xPos, tLp.topMargin, tLp.rightMargin, tLp.bottomMargin);
+        }
+        return this;
+    }
+
+    /**
+     * 设置item的高度
+     */
+    public RecyclerHolder setItemHeight(int height){
+        itemView.getLayoutParams().height = height;
+        return this;
+    }
+
+    /**
+     * 设置子控件的高度
+     */
+    public RecyclerHolder setSize(int viewId, int width, int height){
+        View view = getView(viewId);
+        ViewGroup.LayoutParams lp = view.getLayoutParams();
+        lp.height = height;
+        lp.width = width;
+        view.setLayoutParams(lp);
+        return this;
+    }
+
+    /**
+     * 设置子控件可见性
+     */
+    public void setVisibility(int viewId, int visibility){
+        View view = getView(viewId);
+        view.setVisibility(visibility);
+    }
+
+    /**
+     * 设置字体
+     */
+    public void setTypeFace(int viewId, Typeface typeFace){
+        ((TextView)getView(viewId)).setTypeface(typeFace);
+    }
+
+    /**
+     * 设置控件可见
+     * @param viewId
+     * @param isVisible
+     */
+    public void setVisible(int viewId,boolean isVisible){
+        getView(viewId).setVisibility(isVisible?View.VISIBLE: View.GONE);
+    }
+}
